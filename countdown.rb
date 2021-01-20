1ce30f3901b14bb05d78471f115653d6853083d0
Gabriel Lopez <gabelopez84@gmail.com>
2021-01-20 13:42:37 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
+def countdown(num)
+    while num > 0
+        puts "#{num} SECOND(S)!"
+        num -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
 
-def countdown
+
+def countdown_with_sleep(i_love_pasta)
+while i_love_pasta > 0
+    sleep(5)
+    puts "#{i_love_pasta} SECOND(S)!"
+    i_love_pasta -= 1
+end
+return "HAPPY NEW YEAR!"
 end
